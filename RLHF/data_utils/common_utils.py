2582f792b22f3c3baf8100f3ee2168833f6b0283
jackyk02 <jackykwok@berkeley.edu>
2025-01-24 02:01:03 +0000

    token level action processing

--- RLHF/data_utils/common_utils.py
+++ RLHF/data_utils/common_utils.py
@@ -396,22 +396,17 @@ def preprocess_v1(
         )
     # Tokenize conversations
     #process action inputs
-    def extract_array(s):
-        import re
-        # Find all numbers in square brackets
-        matches = re.findall(r'\[([\d, ]+)\]', s)
-        if matches:
-            # Convert the first match into a list of integers
-            return [int(num) for num in matches[0].split(',')]
-        return []
+    def extract_number(text):
+        target_line = [line for line in text.split('\n') if "robot should take the action:" in line][0]
+        return int(target_line.strip().split(':')[-1].strip())
 
     new_conversations = []
     action_ids = []
     for prompt in conversations:
-        id = extract_array(prompt)
+        id = extract_number(prompt)
         action_ids.append(id)
         # hello is 22172
-        prompt = prompt.replace(str(id), "hello hello hello hello hello hello hello ")
+        prompt = prompt.replace(str(id), "hello ")
         new_conversations.append(prompt)
     conversations = new_conversations
 
@@ -432,12 +427,13 @@ def preprocess_v1(
             truncation=True,
         ).input_ids
     
-    # print("input_ids!!!", input_ids)
+    print("input_ids!!!", input_ids)
     repeated_indices = (input_ids == 22172).nonzero()
-    start_idx = repeated_indices[0][1].item()  # Get the first occurrence
-    end_idx = repeated_indices[-1][1].item() + 1  # Get the last occurrence + 1
-    input_ids[0, start_idx:end_idx] = torch.tensor(np.array(action_ids[0])-1000)
-    # print("input_ids after:", input_ids)
+    idx = repeated_indices[0][1].item()  # Get the single occurrence
+    input_ids[0, idx] = torch.tensor(action_ids[0]-1000)  # No need for -1000 offset
+    print("input_ids after:", input_ids)
+    import sys 
+    sys.exit()
 
     targets = input_ids.clone()
     validity = [True] * len(input_ids)
